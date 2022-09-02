b631d1ca3aeb15e4ad21c633184c8b3a72df034d
AndreiDukhouski <111515010+AndreiDukhouski@users.noreply.github.com>
2022-09-03 00:39:00 +0300

    Add files via upload

--- HomeWork_2/index.js
+++ HomeWork_2/index.js
@@ -1,26 +1,5 @@
-let rotating = [
-    {opacity: 1},
-    {opacity: 0} 
-];
+document.querySelector('.collapsible__button').addEventListener('click', function (e) {
+  var div = document.querySelector('.collapsible__content')
+  div.style.display = div.style.display === 'none' ? 'block' : 'none'
+})
 
-let timing = {
-    fill: 'forwards'
-};
-let timing2 = {
-    duration: 100,
-    fill: 'backwards'
-};
-
-function hid() {
-document.querySelector('.collapsible__content').animate(
-    rotating,
-    timing
-)
-}
-
-function vis() {
-document.querySelector('.collapsible__content').animate(
-    rotating,
-    timing2
-).pause()
-}
